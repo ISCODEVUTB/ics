71bf89bc269a42d5682c574d5094b02d2523f96d
RobertPC75 <98894368+RobertPC75@users.noreply.github.com>
2023-05-17 18:23:32 -0500

    Update

--- services/createEvent.py
+++ services/createEvent.py
@@ -1,7 +1,5 @@
 from models.models import EventIn
 import json
-import string
-import random
 from services.constans import dataPath
 
 # Function to create a new event object in a JSON database
@@ -12,16 +10,6 @@ def create_event(event: EventIn):
 
     # Adding the new event object to the 'events' list in the database
     data["events"].append(event.dict())
-
-    """number_of_strings = 5
-    length_of_string = 8
-    for x in range(number_of_strings):
-        credential= ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length_of_string))
-
-        
-    event_name = event.dict()["name"]
-    credential_entry = {"name": event_name, "credential": credential}
-    data["credentials"].append(credential_entry)"""
 
     # Saving the updated database to the JSON file
     with open(dataPath, "w") as f:
